708d25050f0c17fda7483a3378dbd636f0b2c35c
yuma1217 <yu.matsumura1217@gmail.com>
2022-07-07 13:05:23 +0900

    Init

---
+++ SwiftUISample/SwiftUISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISampleApp.swift
+//  SwiftUISample
+//
+//  Created by 松村 裕 on 2022/07/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
